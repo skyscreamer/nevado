ca0dd2923bc5ea7bb42f96c9dc529c1373d3033e
Carter Page <cpage@pobox.com>
2012-04-02 00:35:27 -0400

    Added test for 3.10

---
+++ src/test/java/org/skyscreamer/nevado/jms/message/ReceivedMessageTest.java
@@ -0,0 +1,56 @@
+package org.skyscreamer.nevado.jms.message;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.skyscreamer.nevado.jms.AbstractJMSTest;
+
+import javax.jms.JMSException;
+import javax.jms.MessageNotWriteableException;
+import javax.jms.TextMessage;
+
+/**
+ * Test for section 3.5.6 of the JMS 1.1 Specification.
+ *
+ * @author Carter Page <carter@skyscreamer.org>
+ */
+public class ReceivedMessageTest extends AbstractJMSTest {
+    @Test
+    public void testInitialState() throws JMSException {
+        TextMessage msg = getSession().createTextMessage();
+        msg.setStringProperty("a", "b");
+        msg.setText("test");
+        msg.setJMSCorrelationID("X");
+        TextMessage msgOut = (TextMessage)sendAndReceive(msg);
+        Assert.assertEquals("b", msgOut.getStringProperty("a"));
+        Assert.assertEquals("test", msgOut.getText());
+        Assert.assertEquals("X", msgOut.getJMSCorrelationID());
+
+        // Test setting header
+        msgOut.setJMSCorrelationID("Y");
+        Assert.assertEquals("Y", msgOut.getJMSCorrelationID());
+
+        // Test setting property
+        try {
+            msgOut.setStringProperty("c", "d");
+            Assert.fail("Should have thrown exception on property write");
+        }
+        catch (MessageNotWriteableException e) {
+            // Expected
+        }
+        msgOut.clearProperties();
+        msgOut.setStringProperty("c", "d");
+        Assert.assertEquals("d", msgOut.getStringProperty("c"));
+
+        // Test setting body
+        try {
+            msgOut.setText("something else");
+            Assert.fail("Should have thrown exception on body write");
+        }
+        catch (MessageNotWriteableException e) {
+            // Expected
+        }
+        msgOut.clearBody();
+        msgOut.setText("new text");
+        Assert.assertEquals("new text", msgOut.getText());
+    }
+}
