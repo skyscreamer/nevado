22605830bb8f6c4fd4caa645068600c8d4a6947f
Carter Page <cpage@pobox.com>
2012-04-23 19:09:51 -0400

    Add messageidutil

---
+++ src/main/java/org/skyscreamer/nevado/jms/util/MessageIdUtil.java
@@ -0,0 +1,14 @@
+package org.skyscreamer.nevado.jms.util;
+
+import java.util.UUID;
+
+/**
+ * Generate nevado-specific message ID's when SQS message ID's aren't sufficient
+ *
+ * @author Carter Page <carter@skyscreamer.org>
+ */
+public class MessageIdUtil {
+    public static String createMessageId() {
+        return "nevado-" + UUID.randomUUID().toString();
+    }
+}
