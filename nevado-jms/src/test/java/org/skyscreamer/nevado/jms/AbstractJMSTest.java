fe5cecd69d87eafa555fc6377101d6ad0cd45415
Carter Page <cpage@wgen.net>
2012-05-18 18:46:17 -0400

    Make AbstractJMSTest forgiving of an absent aws.properties file when testing against the mock connector.

--- nevado-jms/src/test/java/org/skyscreamer/nevado/jms/AbstractJMSTest.java
+++ nevado-jms/src/test/java/org/skyscreamer/nevado/jms/AbstractJMSTest.java
@@ -86,7 +86,7 @@ public abstract class AbstractJMSTest {
 
     private void initializeAWSCredentials() throws IOException {
         Properties prop = new Properties();
-        InputStream in = getClass().getResourceAsStream("/aws.properties");
+        InputStream in = getClass().getResourceAsStream("/aws.properties.bak");
         prop.load(in);
         in.close();
 
@@ -95,14 +95,14 @@ public abstract class AbstractJMSTest {
         if (_awsAccessKey == null || _awsAccessKey.trim().length() == 0
             || _awsSecretKey == null || _awsSecretKey.trim().length() == 0) {
                 System.out.println("ATTENTION: You have not set up your AWS credentials.  Follow thes steps:\n" +
-                        "    1. Copy nevado-jms/src/test/resources/aws.properties.TEMPLATE to\n" +
-                        "       nevado-jms/src/test/resources/aws.properties\n" +
-                        "    2. Edit aws.properties with your access keys from\n" +
+                        "    1. Copy nevado-jms/src/test/resources/aws.properties.bak.TEMPLATE to\n" +
+                        "       nevado-jms/src/test/resources/aws.properties.bak\n" +
+                        "    2. Edit aws.properties.bak with your access keys from\n" +
                         "       https://aws-portal.amazon.com/gp/aws/securityCredentials\n" +
                         "    3. Have git ignore the new file.  Add the following line to .git/info/exclude:\n\n" +
-                        "        aws.properties\n\n" +
+                        "        aws.properties.bak\n\n" +
                         "*** Keep your keys in a safe place and don't commit them to source control. ***\n\n");
-            throw new MissingResourceException("Resource /aws.properties does not exist",
+            throw new MissingResourceException("Resource /aws.properties.bak does not exist",
                     null, null);
         }
     }
