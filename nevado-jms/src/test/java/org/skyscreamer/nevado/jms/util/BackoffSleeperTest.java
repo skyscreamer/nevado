7b4d25dba88e6410d9abf52790b224f17640ab82
Carter Page <cpage@pobox.com>
2013-09-30 12:08:23 -0400

    Update BackoffSleeperTest.java

--- nevado-jms/src/test/java/org/skyscreamer/nevado/jms/util/BackoffSleeperTest.java
+++ nevado-jms/src/test/java/org/skyscreamer/nevado/jms/util/BackoffSleeperTest.java
@@ -9,26 +9,23 @@ import org.junit.Test;
  * @author Carter Page <carter@skyscreamer.org>
  */
 public class BackoffSleeperTest {
-    // Cloudbees needs coarse resolution because their boxes have heavy contention
-    public static final int ACCEPTABLE_DELTA_MS = 50;
-
     @Test
     public void testBackoffSleeper() throws InterruptedException {
         BackoffSleeper sleeper = new BackoffSleeper(50, 230, 2.0);
         long now = System.currentTimeMillis();
         sleeper.sleep();
-        Assert.assertEquals(now + 50, System.currentTimeMillis(), ACCEPTABLE_DELTA_MS);
+        Assert.assertEquals(now + 50, System.currentTimeMillis(), 50);
         sleeper.sleep();
-        Assert.assertEquals(now + 150, System.currentTimeMillis(), ACCEPTABLE_DELTA_MS);
+        Assert.assertEquals(now + 150, System.currentTimeMillis(), 100);
         sleeper.sleep();
-        Assert.assertEquals(now + 350, System.currentTimeMillis(), ACCEPTABLE_DELTA_MS);
+        Assert.assertEquals(now + 350, System.currentTimeMillis(), 100);
         sleeper.sleep();
-        Assert.assertEquals(now + 580, System.currentTimeMillis(), ACCEPTABLE_DELTA_MS);
+        Assert.assertEquals(now + 580, System.currentTimeMillis(), 100);
         sleeper.sleep();
-        Assert.assertEquals(now + 810, System.currentTimeMillis(), ACCEPTABLE_DELTA_MS);
+        Assert.assertEquals(now + 810, System.currentTimeMillis(), 100);
         sleeper.reset();
-        Assert.assertEquals(now + 810, System.currentTimeMillis(), ACCEPTABLE_DELTA_MS);
+        Assert.assertEquals(now + 810, System.currentTimeMillis(), 100);
         sleeper.sleep();
-        Assert.assertEquals(now + 860, System.currentTimeMillis(), ACCEPTABLE_DELTA_MS);
+        Assert.assertEquals(now + 860, System.currentTimeMillis(), 100);
     }
 }
