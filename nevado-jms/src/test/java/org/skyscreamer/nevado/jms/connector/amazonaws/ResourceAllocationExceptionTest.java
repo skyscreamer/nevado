3c221b6b7380ca1891d3f265aa3fe72805a5bc2f
Carter Page <carter@skyscreamer.org>
2014-01-11 11:11:16 -0500

    Removed Typica dependencies. Changed version to 1.3.0-SNAPSHOT. Updated ResourceAllocationExceptionTest.

---
+++ nevado-jms/src/test/java/org/skyscreamer/nevado/jms/connector/amazonaws/ResourceAllocationExceptionTest.java
@@ -0,0 +1,35 @@
+package org.skyscreamer.nevado.jms.connector.amazonaws;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.skyscreamer.nevado.jms.AbstractJMSTest;
+import org.skyscreamer.nevado.jms.NevadoConnectionFactory;
+import org.skyscreamer.nevado.jms.connector.amazonaws.AmazonAwsSQSConnector;
+import org.skyscreamer.nevado.jms.util.RandomData;
+
+import javax.jms.*;
+
+/**
+ * Test ResourceAllocationException
+ *
+ * @author Carter Page <carter@skyscreamer.org>
+ */
+public class ResourceAllocationExceptionTest extends AbstractJMSTest {
+    @Test(expected = ResourceAllocationException.class)
+    public void testSQSResourceAllocationException() throws JMSException {
+        if (getConnection().getSQSConnector() instanceof AmazonAwsSQSConnector) {
+            NevadoConnectionFactory connectionFactory = new NevadoConnectionFactory(_sqsConnectorFactory);
+            connectionFactory.setAwsSQSEndpoint("a.deliberately.invalid.server");
+            createConnection(connectionFactory);
+        }
+    }
+
+    @Test(expected = ResourceAllocationException.class)
+    public void testSNSResourceAllocationException() throws JMSException {
+        if (getConnection().getSQSConnector() instanceof AmazonAwsSQSConnector) {
+            NevadoConnectionFactory connectionFactory = new NevadoConnectionFactory(_sqsConnectorFactory);
+            connectionFactory.setAwsSNSEndpoint("a.deliberately.invalid.server");
+            createConnection(connectionFactory);
+        }
+    }
+}
