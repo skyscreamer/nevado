0c6610f55d4c1df32155acb9ef96ac4d3f4a7ae8
tejas1990 <tejasraorane9@gmail.com>
2014-07-03 14:45:02 +0530

    Update AmazonAwsSQSConnector.java
    Made changes as per your suggestions. Created my own constructor.

--- nevado-jms/src/main/java/org/skyscreamer/nevado/jms/connector/amazonaws/AmazonAwsSQSConnector.java
+++ nevado-jms/src/main/java/org/skyscreamer/nevado/jms/connector/amazonaws/AmazonAwsSQSConnector.java
@@ -54,6 +54,29 @@ public class AmazonAwsSQSConnector extends AbstractSQSConnector {
         this(awsAccessKey, awsSecretKey, isSecure, receiveCheckIntervalMs, false, 0);
     }
 
+    public AmazonAwsSQSConnector(String awsAccessKey, String awsSecretKey, boolean isSecure, long receiveCheckIntervalMs, boolean isAsync) {
+        super(receiveCheckIntervalMs, isAsync);
+        AWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        String proxyHost = System.getProperty("http.proxyHost");
+        String proxyPort = System.getProperty("http.proxyPort");
+        if(proxyHost != null){
+            clientConfiguration.setProxyHost(proxyHost);
+            if(proxyPort != null){
+              clientConfiguration.setProxyPort(Integer.parseInt(proxyPort));
+            }
+        }  
+        clientConfiguration.setProtocol(isSecure ? Protocol.HTTPS : Protocol.HTTP);
+        if (isAsync) {
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            _amazonSQS = new AmazonSQSAsyncClient(awsCredentials, clientConfiguration, executorService);
+            _amazonSNS = new AmazonSNSAsyncClient(awsCredentials, clientConfiguration, executorService);
+        } else {
+            _amazonSQS = new AmazonSQSClient(awsCredentials, clientConfiguration);
+            _amazonSNS = new AmazonSNSClient(awsCredentials, clientConfiguration);
+        }
+    }
+    
     public AmazonAwsSQSConnector(String awsAccessKey, String awsSecretKey, boolean isSecure, long receiveCheckIntervalMs, boolean isAsync, int visibilityTimeoutOnReset) {
         super(receiveCheckIntervalMs, isAsync, visibilityTimeoutOnReset);
         AWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
